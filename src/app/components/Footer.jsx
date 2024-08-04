5ba5127ecc1ecf0612befedf0a66a3eff5f20b48
XtremeThaqi <erblinthaqi40@gmail.com>
2024-08-05 00:06:45 +0200

    fixed some bugs

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className="px-24 max-lg:px-10 h-[300px] w-full bg-[rgba(31,31,31,0.3)]">
                 <div className="flex gap-32 py-5 max-md:flex-col max-md:gap-10">
                     <div className="relative">
-                        <h2 className="text-2xl text-orange-600 before:absolute before:top-7 before:bg-orange-600 before:w-32 before:h-[2px]">"BLINI"</h2>
+                        <h2 className="text-2xl text-orange-600 before:absolute before:top-7 before:bg-orange-600 before:w-32 before:h-[2px]">&quot;BLINI&quot;</h2>
                     </div>
                     <div className="flex flex-wrap justify-start gap-32 max-md:gap-20 max-sm:flex-col max-sm:gap-10">
                         <div className="text-2xl text-orange-600">
