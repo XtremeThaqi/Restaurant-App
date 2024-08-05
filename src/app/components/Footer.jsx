3187572b9b9ea00a773d9debfb0252dad20bcf1d
XtremeThaqi <erblinthaqi40@gmail.com>
2024-08-05 13:50:32 +0200

    fixed footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,16 +3,16 @@
 export default function Footer() {
     return (
         <>
-            <div className="px-24 max-lg:px-10 h-[300px] w-full bg-[rgba(31,31,31,0.3)]">
-                <div className="flex gap-32 py-5 max-md:flex-col max-md:gap-10">
+            <div className="flex flex-col px-10 py-2 items-center w-full bg-[rgba(31,31,31,0.3)]">
+                <div className="flex flex-wrap gap-20">
                     <div className="relative">
-                        <h2 className="text-2xl text-orange-600 before:absolute before:top-7 before:bg-orange-600 before:w-32 before:h-[2px]">&quot;BLINI&quot;</h2>
+                        <h2 className="text-2xl text-orange-600 before:absolute before:top-7 before:bg-orange-600 before:w-28 before:h-[2px]">&quot;BLINI&quot;</h2>
                     </div>
-                    <div className="flex flex-wrap justify-start gap-32 max-md:gap-20 max-sm:flex-col max-sm:gap-10">
+                    <div className="flex flex-wrap gap-20 ml-52 max-xl:ml-5 max-sm:gap-10">
                         <div className="text-2xl text-orange-600">
                             <h2>Address</h2>
                             <p className="text-[16px] text-zinc-400">Rr. Arben Ahmeti nr: 18</p>
-                            <p className="text-[16px] text-zinc-400">Lipjan, Kosovo</p>
+                            <p className="text-[16px] ">Lipjan, Kosovo</p>
                         </div>
                         <div className="text-2xl text-orange-600">
                             <h2>Details</h2>
@@ -25,12 +25,12 @@ export default function Footer() {
                             <p className="text-[16px] -mt-2 text-zinc-400">erblinthaqi40@gmail.com</p>
                         </div>
                     </div>
+                    
                 </div>
-                <div className="flex justify-center items-end py-5 mt-24 max-md:mt-10 max-sm:mt-0 max-md:py-2">
-                    <div className="flex gap-5">
-                        <p className="text-orange-600 text-[13px]">&copy; Copyright 2024 Powered By <span className="underline italic">BLINI</span></p>
+                <div className="text-center mt-12">
+                        <p className="text-orange-600 text-[13px]">&copy; Copyright 2024 Powered by <span className="underline italic"> Blini</span></p>
+
                     </div>
-                </div>
             </div>
         </>
     )
